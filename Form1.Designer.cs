354bb465b7e7b78a1dfa3498d1b9b31429e4f7fb
Franz Lorenz <freelancer_franzlorenz@web.de>
2020-05-22 16:39:48 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace DeEnrypt
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose( bool disposing )
+      {
+         if ( disposing && ( components != null ) )
+         {
+            components.Dispose();
+         }
+         base.Dispose( disposing );
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+         this.groupBox1 = new System.Windows.Forms.GroupBox();
+         this.labelFile = new System.Windows.Forms.Label();
+         this.buttonSelectFile = new System.Windows.Forms.Button();
+         this.buttonStart = new System.Windows.Forms.Button();
+         this.groupBox1.SuspendLayout();
+         this.SuspendLayout();
+         // 
+         // groupBox1
+         // 
+         this.groupBox1.Controls.Add(this.buttonSelectFile);
+         this.groupBox1.Controls.Add(this.labelFile);
+         this.groupBox1.Location = new System.Drawing.Point(16, 16);
+         this.groupBox1.Name = "groupBox1";
+         this.groupBox1.Size = new System.Drawing.Size(784, 104);
+         this.groupBox1.TabIndex = 0;
+         this.groupBox1.TabStop = false;
+         this.groupBox1.Text = "File to decrypt/encrypt";
+         // 
+         // labelFile
+         // 
+         this.labelFile.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+         this.labelFile.Location = new System.Drawing.Point(8, 48);
+         this.labelFile.Name = "labelFile";
+         this.labelFile.Size = new System.Drawing.Size(640, 48);
+         this.labelFile.TabIndex = 0;
+         this.labelFile.Text = "<>";
+         this.labelFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+         // 
+         // buttonSelectFile
+         // 
+         this.buttonSelectFile.Location = new System.Drawing.Point(664, 48);
+         this.buttonSelectFile.Name = "buttonSelectFile";
+         this.buttonSelectFile.Size = new System.Drawing.Size(104, 48);
+         this.buttonSelectFile.TabIndex = 1;
+         this.buttonSelectFile.Text = "&Select";
+         this.buttonSelectFile.UseVisualStyleBackColor = true;
+         this.buttonSelectFile.Click += new System.EventHandler(this.buttonSelectFile_Click);
+         // 
+         // buttonStart
+         // 
+         this.buttonStart.Location = new System.Drawing.Point(24, 128);
+         this.buttonStart.Name = "buttonStart";
+         this.buttonStart.Size = new System.Drawing.Size(640, 40);
+         this.buttonStart.TabIndex = 1;
+         this.buttonStart.Text = "Start";
+         this.buttonStart.UseVisualStyleBackColor = true;
+         this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(815, 193);
+         this.Controls.Add(this.buttonStart);
+         this.Controls.Add(this.groupBox1);
+         this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+         this.Name = "Form1";
+         this.Text = "De-/Encrypt (c) Franz Lorenz";
+         this.groupBox1.ResumeLayout(false);
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.GroupBox groupBox1;
+      private System.Windows.Forms.Button buttonSelectFile;
+      private System.Windows.Forms.Label labelFile;
+      private System.Windows.Forms.Button buttonStart;
+   }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DeEnrypt
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+      }
+
+      /**
+       * This function is called, if the user wants to 
+       * select a file by the file explorer.
+       */
+      private void buttonSelectFile_Click( object sender, EventArgs e )
+      {
+         OpenFileDialog Dlg = new OpenFileDialog();
+         Dlg.Title = "Select File for Crypt/Decrypt";
+         if( DialogResult.OK == Dlg.ShowDialog() )
+         {
+            labelFile.Text = Dlg.FileName;
+         }
+      }
+
+      /**
+       */
+      private string Encrypt( string sSrcFile )
+      {
+         string sDstFile = "";
+         //
+         if( sSrcFile.EndsWith( ".crypt" ) )                //encrypted file?
+         {                                                  // yes, then...
+            sDstFile = sSrcFile.Substring( 0, sSrcFile.Length - 6 );
+         }
+         else
+         {
+            sDstFile = sSrcFile+".crypt";
+         }
+         //
+         Stream   strDst = File.OpenWrite( sDstFile );
+         Stream   strSrc = File.OpenRead( sSrcFile );
+         byte[]   abBuffer = new byte[32768];
+         int      iBytesRead;
+         //
+         while( ( iBytesRead = strSrc.Read( abBuffer, 0, abBuffer.Length ) ) > 0 )
+         {
+            for( int iIdx=0; iIdx<iBytesRead; iIdx++ )
+            {
+               abBuffer[iIdx] ^= 0xFF;
+            }
+            strDst.Write( abBuffer, 0, iBytesRead );
+         }
+         strDst.Close();
+         strSrc.Close();
+         //
+         return sDstFile;
+      }
+
+
+      private void buttonStart_Click( object sender, EventArgs e )
+      {
+         string sFile = labelFile.Text;
+         if ( File.Exists( sFile ) )                        //does file exists?
+         {                                                  // yes, then...
+            sFile = Encrypt( sFile );                       // run
+            MessageBox.Show( "The file '"+sFile+"' was created.",
+               "Encrypt/Decrypt Success",
+               MessageBoxButtons.OK,
+               MessageBoxIcon.Information );
+         }
+         else                                               //otherwise
+         {                                                  // then...
+            MessageBox.Show( "The file '"+sFile+"' is not readable!",
+               "File Access Error", 
+               MessageBoxButtons.OK, 
+               MessageBoxIcon.Error );
+         }
+      }
+   }
+}
